992835c3a15d7b3877f9debdf1288f6a1733966f
John Adapon <john.adapon@gmail.com>
2014-10-17 13:41:43 -0700

    Add a beautiful, functional, colorful toolbar

---
+++ BLCAwesomeFloatingToolbar.h
@@ -0,0 +1,28 @@
+//
+//  BLCAwesomeFloatingToolbar.h
+//  BlocBrowser revisit
+//
+//  Created by John Patrick Adapon on 10/16/14.
+//  Copyright (c) 2014 John Adapon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCAwesomeFloatingToolbar;
+
+@protocol BLCAwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+-(void) floatingToolbar:(BLCAwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface BLCAwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <BLCAwesomeFloatingToolbarDelegate> delegate;
+
+@end
