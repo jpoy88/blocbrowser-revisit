392921f6e83f3b1cd4e49ce8d052ea8606b94019
John Adapon <john.adapon@gmail.com>
2014-10-16 17:50:27 -0700

    Functional web browser

---
+++ BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser revisit
+//
+//  Created by John Patrick Adapon on 10/16/14.
+//  Copyright (c) 2014 John Adapon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
